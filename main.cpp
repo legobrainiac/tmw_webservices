46137d05b4124c4c8ee3adcf1925cd927ab2dd86
legobrainiac <tomas.antonio.sp@gmail.com>
2019-04-27 13:57:40 +0200

    Support for single server commands implemented.

--- main.cpp
+++ main.cpp
@@ -8,16 +8,16 @@
 #include <iostream>
 #include <string>
 
-#define KEY "tmw159"
+#define KEY ""
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 1
+#define VERSION_MINOR 2
 
 int main()
 {
 	std::cout << "TMW Webservices v" << VERSION_MAJOR << "." << VERSION_MINOR << std::endl;
 	crow::SimpleApp app;
 	
-	auto cmdRt = [](std::string key, std::string cmd) -> crow::json::wvalue
+	auto cmdAll = [](std::string key, std::string cmd) -> crow::json::wvalue
 	{
 		crow::json::wvalue result;
 		
@@ -27,9 +27,8 @@ int main()
 			return result;
 		}
 		
-		std::string cmdStr = "sudo sh ";
+		std::string cmdStr = "sudo sh servercommands_all.sh ";
 		cmdStr += cmd;
-		cmdStr += ".sh";
 		
 		int i = system(cmdStr.c_str());
 		
@@ -38,7 +37,32 @@ int main()
 		return result;
 	};
 	
-	CROW_ROUTE(app, "/<string>/exec/<string>/")(cmdRt);
+	auto cmdSpecific = [](std::string key, int id, std::string cmd) -> crow::json::wvalue
+	{
+		crow::json::wvalue result;
+		
+		if(key != KEY) 
+		{
+			result["apiKey"] = "invalid";
+			return result;
+		}
+		
+		// After building the string, it should look something like this: 
+		//  sudo sh servercommands.sh <id> <command>
+		std::string cmdStr = "sudo sh servercommands.sh ";
+		cmdStr += std::to_string(id);
+		cmdStr += " ";
+		cmdStr += cmd;
+		
+		int i = system(cmdStr.c_str());
+		
+		result["apiKey"] = "valid";
+		result["script_return"] = std::to_string(i);
+		return result;
+	};
+	
+	CROW_ROUTE(app, "/<string>/all/<string>")(cmdAll);
+	CROW_ROUTE(app, "/<string>/<int>/<string>")(cmdSpecific);
 	
 	app.port(8080).multithreaded().run();
 }
